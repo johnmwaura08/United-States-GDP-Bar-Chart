e70c485a4d2d2801e5807f10c9728c9ffccc6d56
John Mwaura <johnmwaura08@gmail.com>
2020-09-09 10:51:34 -0500

    added svg and rect

---
+++ main.js
@@ -0,0 +1,64 @@
+//1.get data from api
+
+
+//dummy data 
+
+const dummy = [
+    [
+        "1947-01-01",
+        243.1
+      ],
+      [
+        "1947-04-01",
+        246.3
+      ],
+      [
+        "1947-07-01",
+        250.1
+      ],
+      [
+        "1947-10-01",
+        260.3
+      ],
+      [
+        "1948-01-01",
+        266.2
+      ],
+      [
+        "1948-04-01",
+        272.9
+      ],
+      [
+        "1948-07-01",
+        279.5
+      ],
+      [
+        "1948-10-01",
+        280.7
+      ],
+      [
+        "1949-01-01",
+        275.4
+      ],
+      [
+        "1949-04-01",
+        271.7
+      ]
+]
+
+const width = 500;
+const height = 500;
+
+const svg = d3.select('body').append('svg')
+            .attr('width', width)
+            .attr('height', height)
+
+
+    svg.selectAll('rect')
+        .data(dummy)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * 22)
+        .attr('y', 0)
+        .attr('width', 20)
+        .attr('height', 100)
