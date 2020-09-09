6b9f4880d009b6c05af85bf61c461c18398e7c43
John Mwaura <johnmwaura08@gmail.com>
2020-09-09 11:32:06 -0500

    removed filter and adjusted height

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 //1.get data from api
+//2. remove last 3 quarters
+//3. width for the bars
+//4 width for svg to fit bars
+//5 scale the heights of the bars
 
 //dummy data
 
@@ -27,6 +31,12 @@ function createBarChart(data) {
   const width = 500;
   const height = 500;
 
+  const barWidth = width / data.length ;
+
+  const scale = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d[1])])
+                .range([0, height])
+
   const svg = d3
     .select("body")
     .append("svg")
@@ -38,8 +48,8 @@ function createBarChart(data) {
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * 22)
-    .attr("y", (d) => height - d[1])
-    .attr("width", 20)
-    .attr("height", (d) => d[1] + "px");
+    .attr("x", (d, i) => i * barWidth  )
+    .attr("y", (d) => height - scale(d[1]))
+    .attr("width", barWidth)
+    .attr("height", (d) => scale(d[1]) + "px");
 }
