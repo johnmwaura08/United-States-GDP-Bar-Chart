6a0fd9d490ef2c7e089817a82562bafd04eb40b3
John Mwaura <johnmwaura08@gmail.com>
2020-09-09 11:02:05 -0500

    fetch method for getting data from api, svg now showing bar chart with data from api

--- main.js
+++ main.js
@@ -1,64 +1,45 @@
 //1.get data from api
 
-
-//dummy data 
+//dummy data
 
 const dummy = [
-    [
-        "1947-01-01",
-        243.1
-      ],
-      [
-        "1947-04-01",
-        246.3
-      ],
-      [
-        "1947-07-01",
-        250.1
-      ],
-      [
-        "1947-10-01",
-        260.3
-      ],
-      [
-        "1948-01-01",
-        266.2
-      ],
-      [
-        "1948-04-01",
-        272.9
-      ],
-      [
-        "1948-07-01",
-        279.5
-      ],
-      [
-        "1948-10-01",
-        280.7
-      ],
-      [
-        "1949-01-01",
-        275.4
-      ],
-      [
-        "1949-04-01",
-        271.7
-      ]
-]
+  ["1947-01-01", 243.1],
+  ["1947-04-01", 246.3],
+  ["1947-07-01", 250.1],
+  ["1947-10-01", 260.3],
+  ["1948-01-01", 266.2],
+  ["1948-04-01", 272.9],
+  ["1948-07-01", 279.5],
+  ["1948-10-01", 280.7],
+  ["1949-01-01", 275.4],
+  ["1949-04-01", 271.7],
+];
 
-const width = 500;
-const height = 500;
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+  .then((res) => res.json())
+  .then((res) => {
+    const { data } = res;
 
-const svg = d3.select('body').append('svg')
-            .attr('width', width)
-            .attr('height', height)
+    createBarChart(data)
+  });
 
+function createBarChart(data) {
+  const width = 500;
+  const height = 500;
 
-    svg.selectAll('rect')
-        .data(dummy)
-        .enter()
-        .append('rect')
-        .attr('x', (d, i) => i * 22)
-        .attr('y', d => height - d[1])
-        .attr('width', 20)
-        .attr('height', d =>  d[1] + 'px')
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 22)
+    .attr("y", (d) => height - d[1])
+    .attr("width", 20)
+    .attr("height", (d) => d[1] + "px");
+}
